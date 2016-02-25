73a5ca5d8905cca2c4f65092d6d368d46a89d075
Felix Thea <felixthea@gmail.com>
2016-02-25 18:58:50 -0500

    improved logging

--- bot.js
+++ bot.js
@@ -4,8 +4,11 @@ var cool = require('cool-ascii-faces');
 var botID = process.env.BOT_ID;
 
 function respond() {
+  console.log("this.req: ",this.req);
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/bot$/;
+
+  console.log("request: ', request")
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
